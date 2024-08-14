717bed6831b4bebfcc397d9a70ef2b2b4f8f174f
Ali Gurbuz <galibekir@gmail.com>
2024-08-14 21:25:07 +0300

    v1.7

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,12 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { collection, addDoc } from "firebase/firestore"; 
+import React,{useState, useEffect} from 'react'
+import { collection, addDoc, getDocs, doc, deleteDoc, updateDoc, collectionGroup } from "firebase/firestore"; 
 import { db } from '../../firebaseConfig';
 import CustomButton from '../components/CustomButton';
 
 
+
 const HomePage = () => {
 
+  const [data, setData] = useState([])
+  // console.log("data:", data)
+  const [isSaved, setIsSaved] = useState(false)
+
+
+  useEffect(() => {
+    getData()
+  
+
+  }, [isSaved])
+  
+
+  //SEND DATA TO FIREFASE
   const sendData = async () => {
     try {
       const docRef = await addDoc(collection(db, "reactNativeLesson"), {
@@ -20,18 +34,83 @@ const HomePage = () => {
     }
   }
 
+  // GET DATA FROM FIREBASE
+  const getData = async () => {
+      const allData = []
 
+      try {
+        const querySnapshot = await getDocs(collection(db, "reactNativeLesson"));
+      querySnapshot.forEach((doc) => {
 
+        allData.push(doc.data())
+        // console.log(`${doc.id} => ${doc.data()}`);
+      });
+        setData(allData) 
+      } catch (error) {
+        console.log(error);
+      }
+      
+  }
+
+  // DELETE DATA FROM FIREBASE
+  const deleteData = async () => {
+    
+      await deleteDoc(doc(db, "reactNativeLesson", ""));
+  }
+  // UPDATE DATA FROM FIREBASE
+  const updateData = async () => {
+    try {
+      const lessonData = doc(db, "reactNativeLesson", "t1lQ62E0k1nilc5rTVhF");
+      // Set the "capital" field of the city 'DC'
+      await updateDoc(lessonData, {
+        lesson: 14
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text>HomePage</Text>
+      {data.map((value, index)=> {
+        return (
+          <View key={index}>
+            <Text>ID: {index}</Text>
+            <Text>{value.title}</Text>
+            <Text>{value.content}</Text>
+            <Text>{value.lesson}</Text>
+          </View>
+        )
+      })}
+
       <CustomButton
         buttonText= {"Save"}
         setWidth={"40%"}
         buttonColor={"blue"}
         pressedButtonColor={'gray'}
-        handleOnpress={sendData}
+        handleOnpress={()=>{sendData(), setIsSaved(isSaved===false ? true : false )}} 
          />
+      <CustomButton
+        buttonText= {"Get Data"}
+        setWidth={"40%"}
+        buttonColor={"blue"}
+        pressedButtonColor={'gray'}
+        handleOnpress={getData}
+         />
+      <CustomButton
+        buttonText= {"Delete Data"}
+        setWidth={"40%"}
+        buttonColor={"blue"}
+        pressedButtonColor={'gray'}
+        handleOnpress={deleteData}
+         />
+      <CustomButton
+        buttonText= {"Update Data"}
+        setWidth={"40%"}
+        buttonColor={"blue"}
+        pressedButtonColor={'gray'}
+        handleOnpress={updateData}
+         />       
     </View>
   )
 }
